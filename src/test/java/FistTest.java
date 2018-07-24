2d4751a2407f5bb24df74581c920e2b427dd6963
adikalova <adikalova@gmail.com>
2018-07-24 16:32:30 +0300

    added LoginTest

--- src/test/java/FistTest.java
+++ src/test/java/FistTest.java
@@ -1,26 +1,33 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class FistTest {
-    protected static WebDriver driver;
+    public static WebDriver driver;
 
     @BeforeTest
     public void startChrome() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\adikalova\\Desktop\\JustTestIt\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\adikalova\\Desktop\\JustTestIt\\enviroment\\chromedriver.exe");
 
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-    }
-
-    @Test(priority = 1, description = "Successful login into Demoqa")
-    public void SuccessfulLoginToDemoqa() {
         driver.get("http://demoqa.com/");
-
-
     }
-}
+
+    @Test(priority = 1 )
+    public void LoginToDemoqa() throws InterruptedException {
+        driver.get("http://demoqa.com/login");
+        driver.findElement(By.id(("user_login"))).sendKeys("anastasiya");
+        driver.findElement(By.id("user_pass")).sendKeys("A771d6552");
+        driver.findElement(By.id("wp-submit")).click();
+
+        Assert.assertTrue(driver.getTitle().contains("Demoqa"), "Login is successful!");
+    }
+    }
+
